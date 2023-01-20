4f0d4e8c9bf09777aea7f79283e71ff0fbd71e01
Aditya Kumar SIngh <adityasingh9501@gmail.com>
2023-01-20 21:47:03 +0530

    Basic structure of app completed

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+
+  recipes : Recipe[] = [
+
+    new Recipe(
+      'Test Recipe',
+      'Test Description',
+      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'
+    ),
+
+    new Recipe(
+      'Test Recipe',
+      'Test Description',
+      'https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg'
+    )
+
+  ];
+}
